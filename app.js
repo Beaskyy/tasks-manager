17600b65a5904f2c157fa20d77af1f21c6bb26b1
Beaskyy <afeez.akintobi@yahoo.com>
2024-07-06 12:37:14 +0100

    create, delete, and get tasks

--- app.js
+++ app.js
@@ -1,14 +1,27 @@
 const express = require("express");
 const app = express();
+require("dotenv").config();
 
 const tasks = require("./routes/tasks");
+const connectDB = require("./db/connect");
+
+require("./db/connect");
 
 app.use(express.json());
 
 app.use("/api/v1/tasks", tasks);
-app.use("/api/v1/tasks/:id", tasks)
+app.use("/api/v1/tasks/:id", tasks);
 const port = 3000;
 
-app.listen(port, () => {
-  console.log(`running on port ${port}..`);
-});
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, () => {
+      console.log(`running on port ${port}..`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
