f7f5f762667b763ce9595734d9a940a695cc20ab
Beaskyy <afeez.akintobi@yahoo.com>
2024-07-29 22:40:01 +0100

    update

--- app.js
+++ app.js
@@ -1,35 +1,35 @@
-const express = require("express");
-const app = express();
-require("dotenv").config();
+// const express = require("express");
+// const app = express();
+// require("dotenv").config();
 
-const tasks = require("./routes/tasks");
-const connectDB = require("./db/connect");
-const notFound = require("./middleware/not-found");
-const errorHandlerMiddleware = require("./middleware/error-handler");
+// const tasks = require("./routes/tasks");
+// const connectDB = require("./db/connect");
+// const notFound = require("./middleware/not-found");
+// const errorHandlerMiddleware = require("./middleware/error-handler");
 
-require("./db/connect");
+// require("./db/connect");
 
-// middleware
-app.use(express.static("./public"));
-app.use(express.json());
+// // middleware
+// app.use(express.static("./public"));
+// app.use(express.json());
 
-app.use("/api/v1/tasks", tasks);
+// app.use("/api/v1/tasks", tasks);
 
-app.use(notFound);
-app.use(errorHandlerMiddleware);
+// app.use(notFound);
+// app.use(errorHandlerMiddleware);
 
-// port
-const port = process.env.PORT || 3000;
+// // port
+// const port = process.env.PORT || 3000;
 
-const start = async () => {
-  try {
-    await connectDB(process.env.MONGO_URI);
-    app.listen(port, () => {
-      console.log(`running on port ${port}..`);
-    });
-  } catch (error) {
-    console.log(error);
-  }
-};
+// const start = async () => {
+//   try {
+//     await connectDB(process.env.MONGO_URI);
+//     app.listen(port, () => {
+//       console.log(`running on port ${port}..`);
+//     });
+//   } catch (error) {
+//     console.log(error);
+//   }
+// };
 
-start();
+// start();
